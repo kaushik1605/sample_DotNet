26871bf4e8c464b0ccf9be2229b906403a41b8ef
Kaushik Narendra <kaushik_narendra@dell.com>
2016-01-11 13:03:08 +0530

    Initial Commit - Kaushik

---
+++ SimpleMusicPlayer/SimpleMusicPlayer/Core/NLogLogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NLog;
+using LogLevel = Splat.LogLevel;
+
+namespace SimpleMusicPlayer.Core
+{
+    internal class NLogLogger : Splat.ILogger
+    {
+        private readonly Logger logger;
+
+        public NLogLogger(Logger logger)
+        {
+            if (logger == null)
+                throw new ArgumentNullException("logger");
+
+            this.logger = logger;
+        }
+
+        public LogLevel Level { get; set; }
+
+        public void Write(string message, LogLevel logLevel)
+        {
+            logger.Log(NLogLogLevelToSplatLogLevel(logLevel), message);
+        }
+
+        private static NLog.LogLevel NLogLogLevelToSplatLogLevel(Splat.LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Debug:
+                    return NLog.LogLevel.Debug;
+
+                case LogLevel.Info:
+                    return NLog.LogLevel.Info;
+
+                case LogLevel.Warn:
+                    return NLog.LogLevel.Warn;
+
+                case LogLevel.Error:
+                    return NLog.LogLevel.Error;
+
+                case LogLevel.Fatal:
+                    return NLog.LogLevel.Fatal;
+                default:
+                    throw new ArgumentOutOfRangeException("logLevel", logLevel, "This Splat.LogLevel isn't implemented!");
+            }
+        }
+    }
+}
---
+++ SimpleMusicPlayer/SimpleMusicPlayer/Views/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using MahApps.Metro.Controls;
+using ReactiveUI;
+using SimpleMusicPlayer.Core;
+using SimpleMusicPlayer.ViewModels;
+
+namespace SimpleMusicPlayer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow, IViewFor<MainViewModel>
+    {
+        public MainWindow(MainViewModel mainViewModel)
+        {
+            this.ViewModel = mainViewModel;
+
+            this.InitializeComponent();
+
+            //this.Title = string.Format("{0} {1}", this.Title, Assembly.GetExecutingAssembly().GetName().Version);
+            this.Title = string.Format("{0}", this.Title);
+
+            this.WhenAnyValue(x => x.ViewModel).BindTo(this, x => x.DataContext);
+
+            this.Events().SourceInitialized.Subscribe(e => this.FitIntoScreen());
+
+            // load playlist and command line stuff
+            this.Events().Loaded.Throttle(TimeSpan.FromMilliseconds(500), RxApp.MainThreadScheduler).InvokeCommand(this.ViewModel.PlayListsViewModel.StartUpCommand);
+
+            this.WhenActivated(d => this.WhenAnyValue(x => x.ViewModel)
+                                        .Subscribe(vm => {
+                                            var previewKeyDown = this.Events().PreviewKeyDown;
+                                            // handle main view keys
+                                            previewKeyDown.Subscribe(vm.HandlePreviewKeyDown);
+                                            // handle playlist keys
+                                            previewKeyDown.Where(x => x.Key == Key.Enter).InvokeCommand(vm.PlayListsViewModel.PlayCommand);
+                                            previewKeyDown.Where(x => x.Key == Key.Delete).InvokeCommand(vm.PlayListsViewModel.DeleteCommand);
+
+                                            var window = Window.GetWindow(this);
+                                            if (window != null)
+                                            {
+                                                vm.PlayListsViewModel.CalcPlayListItemTemplateByActualWidth(window.ActualWidth, window.ActualHeight);
+                                                window.Events().SizeChanged.Throttle(TimeSpan.FromMilliseconds(15), RxApp.MainThreadScheduler).Subscribe(e => vm.PlayListsViewModel.CalcPlayListItemTemplateByActualWidth(e.NewSize.Width, e.NewSize.Height));
+                                            }
+
+                                            this.Events().Closed.InvokeCommand(vm.PlayListsViewModel.FileSearchWorker.StopSearchCmd);
+                                            this.Events().Closed.Subscribe(x => vm.ShutDown());
+                                        }));
+        }
+
+        public override IWindowPlacementSettings GetWindowPlacementSettings()
+        {
+            return this.ViewModel.CustomWindowPlacementSettings;
+        }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MainViewModel), typeof(MainWindow), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainViewModel)value; }
+        }
+    }
+}
